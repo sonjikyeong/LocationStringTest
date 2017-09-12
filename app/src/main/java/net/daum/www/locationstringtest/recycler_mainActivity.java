91565fd4e49fc54d6de402e883d237e2fdec73da
thsjikyeong <thswlrud0@naver.com>
2017-09-12 23:05:03 +0900

    ..

--- app/src/main/java/net/daum/www/locationstringtest/recycler_mainActivity.java
+++ app/src/main/java/net/daum/www/locationstringtest/recycler_mainActivity.java
@@ -8,9 +8,13 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -49,4 +53,24 @@ public class recycler_mainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.fragment_crime_list, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        switch(item.getItemId()){
+            case R.id.menu_item_new_crime:
+                Toast.makeText(this, "new one", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
